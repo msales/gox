fd78aa010568d035ede86a3458291825a475eb15
boguslaw-wojcik <31538120+boguslaw-wojcik@users.noreply.github.com>
2020-05-07 17:12:20 +0200

    Slicex contains speedup for single searches and case insensitive matching method.

--- slicex/contains.go
+++ slicex/contains.go
@@ -1,7 +1,19 @@
 package slicex
 
+import "strings"
+
 // ContainsString checks if all elements from needles list are in the haystack.
 func ContainsString(haystack []string, needles ...string) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
 	checks := make(map[string]struct{}, len(needles))
 	for _, n := range needles {
 		checks[n] = struct{}{}
@@ -21,8 +33,49 @@ func ContainsString(haystack []string, needles ...string) bool {
 	return false
 }
 
+// MatchesString checks if all elements from needles list are in the haystack in a case insensitive manner.
+func MatchesString(haystack []string, needles ...string) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if strings.EqualFold(h, needles[0]) {
+				return true
+			}
+		}
+		return false
+	}
+
+	checks := make(map[string]struct{}, len(needles))
+	for _, n := range needles {
+		checks[strings.ToLower(n)] = struct{}{}
+	}
+
+	for _, h := range haystack {
+		_, ok := checks[strings.ToLower(h)]
+		if ok {
+			delete(checks, h)
+
+			if len(checks) == 0 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
 // ContainsInt checks if all elements from needles list are in the haystack.
 func ContainsInt(haystack []int, needles ...int) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
 	checks := make(map[int]struct{}, len(needles))
 	for _, n := range needles {
 		checks[n] = struct{}{}
@@ -44,6 +97,16 @@ func ContainsInt(haystack []int, needles ...int) bool {
 
 // ContainsInt32 checks if all elements from needles list are in the haystack.
 func ContainsInt32(haystack []int32, needles ...int32) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
 	checks := make(map[int32]struct{}, len(needles))
 	for _, n := range needles {
 		checks[n] = struct{}{}
@@ -65,6 +128,16 @@ func ContainsInt32(haystack []int32, needles ...int32) bool {
 
 // ContainsInt64 checks if all elements from needles list are in the haystack.
 func ContainsInt64(haystack []int64, needles ...int64) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
 	checks := make(map[int64]struct{}, len(needles))
 	for _, n := range needles {
 		checks[n] = struct{}{}
@@ -86,6 +159,16 @@ func ContainsInt64(haystack []int64, needles ...int64) bool {
 
 // ContainsFloat32 checks if all elements from needles list are in the haystack.
 func ContainsFloat32(haystack []float32, needles ...float32) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
 	checks := make(map[float32]struct{}, len(needles))
 	for _, n := range needles {
 		checks[n] = struct{}{}
@@ -107,6 +190,16 @@ func ContainsFloat32(haystack []float32, needles ...float32) bool {
 
 // ContainsFloat64 checks if all elements from needles list are in the haystack.
 func ContainsFloat64(haystack []float64, needles ...float64) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
 	checks := make(map[float64]struct{}, len(needles))
 	for _, n := range needles {
 		checks[n] = struct{}{}
@@ -125,5 +218,3 @@ func ContainsFloat64(haystack []float64, needles ...float64) bool {
 
 	return false
 }
-
-
