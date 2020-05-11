fdb2a2da7ba9f4e2f4b76f1d54385066b8579f75
Hugon Sknadaj <hugon.sknadaj@msales.com>
2020-05-11 10:30:01 +0200

    Extended slicex contains for uint

--- slicex/contains.go
+++ slicex/contains.go
@@ -218,3 +218,96 @@ func ContainsFloat64(haystack []float64, needles ...float64) bool {
 
 	return false
 }
+
+// ContainsUint checks if all elements from needles list are in the haystack.
+func ContainsUint(haystack []uint, needles ...uint) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
+	checks := make(map[uint]struct{}, len(needles))
+	for _, n := range needles {
+		checks[n] = struct{}{}
+	}
+
+	for _, h := range haystack {
+		_, ok := checks[h]
+		if ok {
+			delete(checks, h)
+
+			if len(checks) == 0 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// ContainsUint64 checks if all elements from needles list are in the haystack.
+func ContainsUint64(haystack []uint64, needles ...uint64) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
+	checks := make(map[uint64]struct{}, len(needles))
+	for _, n := range needles {
+		checks[n] = struct{}{}
+	}
+
+	for _, h := range haystack {
+		_, ok := checks[h]
+		if ok {
+			delete(checks, h)
+
+			if len(checks) == 0 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// ContainsUint32 checks if all elements from needles list are in the haystack.
+func ContainsUint32(haystack []uint32, needles ...uint32) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
+	checks := make(map[uint32]struct{}, len(needles))
+	for _, n := range needles {
+		checks[n] = struct{}{}
+	}
+
+	for _, h := range haystack {
+		_, ok := checks[h]
+		if ok {
+			delete(checks, h)
+
+			if len(checks) == 0 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
--- slicex/contains_test.go
+++ slicex/contains_test.go
@@ -354,6 +354,150 @@ func TestContainsFloat64(t *testing.T) {
 	}
 }
 
+func TestContainsUint(t *testing.T) {
+	tests := []struct {
+		name     string
+		slice    []uint
+		contains []uint
+		want     bool
+	}{
+		{
+			name:     "found 1 element",
+			slice:    []uint{1, 2, 3, 4, 5, 6},
+			contains: []uint{3},
+			want:     true,
+		},
+		{
+			name:     "found 3 elements",
+			slice:    []uint{1, 2, 3, 4, 5, 6},
+			contains: []uint{3, 5, 6},
+			want:     true,
+		},
+		{
+			name:     "found some but not all elements",
+			slice:    []uint{1, 2, 3, 4, 5, 6},
+			contains: []uint{3, 5, 6, 7},
+			want:     false,
+		},
+		{
+			name:     "found no elements",
+			slice:    []uint{1, 2, 3, 4, 5, 6},
+			contains: []uint{7, 9},
+			want:     false,
+		},
+		{
+			name:     "found no element",
+			slice:    []uint{1, 2, 3, 4, 5, 6},
+			contains: []uint{7},
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ContainsUint(tt.slice, tt.contains...)
+			if tt.want != got {
+				t.Errorf("Got %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestContainsUint32(t *testing.T) {
+	tests := []struct {
+		name     string
+		slice    []uint32
+		contains []uint32
+		want     bool
+	}{
+		{
+			name:     "found 1 element",
+			slice:    []uint32{1, 2, 3, 4, 5, 6},
+			contains: []uint32{3},
+			want:     true,
+		},
+		{
+			name:     "found 3 elements",
+			slice:    []uint32{1, 2, 3, 4, 5, 6},
+			contains: []uint32{3, 5, 6},
+			want:     true,
+		},
+		{
+			name:     "found some but not all elements",
+			slice:    []uint32{1, 2, 3, 4, 5, 6},
+			contains: []uint32{3, 5, 6, 7},
+			want:     false,
+		},
+		{
+			name:     "found no elements",
+			slice:    []uint32{1, 2, 3, 4, 5, 6},
+			contains: []uint32{7, 9},
+			want:     false,
+		},
+		{
+			name:     "found no element",
+			slice:    []uint32{1, 2, 3, 4, 5, 6},
+			contains: []uint32{7},
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ContainsUint32(tt.slice, tt.contains...)
+			if tt.want != got {
+				t.Errorf("Got %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestContainsUint64(t *testing.T) {
+	tests := []struct {
+		name     string
+		slice    []uint64
+		contains []uint64
+		want     bool
+	}{
+		{
+			name:     "found 1 element",
+			slice:    []uint64{1, 2, 3, 4, 5, 6},
+			contains: []uint64{3},
+			want:     true,
+		},
+		{
+			name:     "found 3 elements",
+			slice:    []uint64{1, 2, 3, 4, 5, 6},
+			contains: []uint64{3, 5, 6},
+			want:     true,
+		},
+		{
+			name:     "found some but not all elements",
+			slice:    []uint64{1, 2, 3, 4, 5, 6},
+			contains: []uint64{3, 5, 6, 7},
+			want:     false,
+		},
+		{
+			name:     "found no elements",
+			slice:    []uint64{1, 2, 3, 4, 5, 6},
+			contains: []uint64{7, 9},
+			want:     false,
+		},
+		{
+			name:     "found no element",
+			slice:    []uint64{1, 2, 3, 4, 5, 6},
+			contains: []uint64{7},
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ContainsUint64(tt.slice, tt.contains...)
+			if tt.want != got {
+				t.Errorf("Got %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
 func BenchmarkContainsString_Single(b *testing.B) {
 	haystack := []string{"a", "b", "c", "d", "e", "f"}
 	b.ResetTimer()
@@ -477,5 +621,59 @@ func BenchmarkContainsFloat64_Multiple(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		ContainsFloat64(haystack, 2, 3)
+	}
+}
+
+func BenchmarkContainsUint_Single(b *testing.B) {
+	haystack := []uint{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsUint(haystack, 3)
+	}
+}
+
+func BenchmarkContainsUint_Multiple(b *testing.B) {
+	haystack := []uint{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsUint(haystack, 2, 3)
+	}
+}
+
+func BenchmarkContainsUint32_Single(b *testing.B) {
+	haystack := []uint32{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsUint32(haystack, 3)
+	}
+}
+
+func BenchmarkContainsUint32_Multiple(b *testing.B) {
+	haystack := []uint32{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsUint32(haystack, 2, 3)
+	}
+}
+
+func BenchmarkContainsUint64_Single(b *testing.B) {
+	haystack := []uint64{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsUint64(haystack, 3)
+	}
+}
+
+func BenchmarkContainsUint64_Multiple(b *testing.B) {
+	haystack := []uint64{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsUint64(haystack, 2, 3)
 	}
 }
