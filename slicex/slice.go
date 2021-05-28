cd9704abbc57cc3b710754ab67251b251444b694
Hugon Sknadaj <hugon94@gmail.com>
2021-05-28 12:25:08 +0200

    Added filter function for slices. (#14)

--- slicex/slice.go
+++ slicex/slice.go
@@ -6,10 +6,7 @@ import "reflect"
 // It panics if x is not a slice.
 func Contains(x interface{}, contains func(i int) bool) bool {
 	v := reflect.ValueOf(x)
-	if v.Kind() != reflect.Slice {
-		panic(&reflect.ValueError{Method: "Contains", Kind: v.Kind()})
-	}
-
+	validateReflection("Contains", reflect.Slice, v)
 	for i := 0; i < v.Len(); i++ {
 		if contains(i) {
 			return true
@@ -17,4 +14,26 @@ func Contains(x interface{}, contains func(i int) bool) bool {
 	}
 
 	return false
+}
+
+// Filter filters out elements from slice.
+// It panics if x is not a pointer to a slice.
+func Filter(x interface{}, filter func(i int) bool) {
+	ptr := reflect.ValueOf(x)
+	validateReflection("Filter", reflect.Ptr, ptr)
+
+	v := ptr.Elem()
+	validateReflection("Filter", reflect.Slice, v)
+
+	for i := 0; i < v.Len(); i++ {
+		if filter(i) {
+			v.Set(reflect.AppendSlice(v.Slice(0, i), v.Slice(i+1, v.Len())))
+		}
+	}
+}
+
+func validateReflection(method string, kind reflect.Kind, v reflect.Value) {
+	if v.Kind() != kind {
+		panic(&reflect.ValueError{Method: method, Kind: v.Kind()})
+	}
 }
