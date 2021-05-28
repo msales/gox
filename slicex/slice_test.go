55b8ac89461ac8cbabd7d966b82be63c386c22b7
Hugon Sknadaj <hugon.sknadaj@msales.com>
2021-05-28 12:06:34 +0200

    Added filter function for slices.

--- slicex/slice_test.go
+++ slicex/slice_test.go
@@ -1,6 +1,7 @@
 package slicex_test
 
 import (
+	"reflect"
 	"testing"
 
 	. "github.com/msales/gox/slicex"
@@ -93,6 +94,118 @@ func BenchmarkContains_Struct(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Contains(haystack, func(i int) bool {
 			return haystack[i].name == "4"
+		})
+	}
+}
+
+func TestFilterInt_Filtered(t *testing.T) {
+	slice := []int{1, 2, 3}
+	want := []int{2, 3}
+	Filter(&slice, func(i int) bool {
+		return slice[i] == 1
+	})
+	if !reflect.DeepEqual(want, slice) {
+		t.Errorf("Got %+v, want %+v", slice, want)
+	}
+}
+
+func TestFilterInt_NotFiltered(t *testing.T) {
+	slice := []int{1, 2, 3}
+	want := []int{1, 2, 3}
+
+	Filter(&slice, func(i int) bool {
+		return slice[i] == 4
+	})
+	if !reflect.DeepEqual(want, slice) {
+		t.Errorf("Got %+v, want %+v", slice, want)
+	}
+}
+
+func TestFilterStruct_Filtered(t *testing.T) {
+	slice := []testStruct{
+		{"1", 1},
+		{"2", 2},
+		{"3", 3},
+	}
+	want := []testStruct{
+		{"2", 2},
+		{"3", 3},
+	}
+	Filter(&slice, func(i int) bool {
+		return slice[i].name == "1"
+	})
+	if !reflect.DeepEqual(want, slice) {
+		t.Errorf("Got %+v, want %+v", slice, want)
+	}
+}
+
+func TestFilterStruct_NotFiltered(t *testing.T) {
+	slice := []testStruct{
+		{"1", 1},
+		{"2", 2},
+		{"3", 3},
+	}
+	want := []testStruct{
+		{"1", 1},
+		{"2", 2},
+		{"3", 3},
+	}
+	Filter(&slice, func(i int) bool {
+		return slice[i].name == "4"
+	})
+	if !reflect.DeepEqual(want, slice) {
+		t.Errorf("Got %+v, want %+v", slice, want)
+	}
+}
+
+func TestFilter_Int_Panic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Contains should panic when not pointer to slice is passed")
+		}
+	}()
+
+	Filter(1, func(i int) bool {
+		return true
+	})
+}
+
+func TestFilter_Slice_Panic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Contains should panic when not pointer to slice is passed")
+		}
+	}()
+
+	Filter([]int{1, 2, 3}, func(i int) bool {
+		return true
+	})
+}
+
+func BenchmarkFilter_Simple(b *testing.B) {
+	haystack := []int32{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Filter(&haystack, func(i int) bool {
+			return haystack[i] == 3 || haystack[i] == 6
+		})
+	}
+}
+
+func BenchmarkFilter_Struct(b *testing.B) {
+	haystack := []testStruct{
+		{"1", 1},
+		{"2", 2},
+		{"3", 3},
+		{"4", 4},
+		{"6", 5},
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Filter(&haystack, func(i int) bool {
+			return haystack[i].name == "3" || haystack[i].value == 4
 		})
 	}
 }
