a92c04d8d45c5ccfe9b2db1fa8ac152a159ab19e
Hugon Sknadaj <hugon.sknadaj@msales.com>
2022-08-12 14:46:47 +0200

    [TASK] TRK-496 Improve gofp package.

---
+++ gofp/contains_test.go
@@ -0,0 +1,182 @@
+package gofp_test
+
+import (
+	"testing"
+
+	. "github.com/msales/gox/gofp"
+)
+
+func TestContains(t *testing.T) {
+	tests := []struct {
+		name     string
+		slice    []int64
+		contains int64
+		want     bool
+	}{
+		{
+			name:     "found element",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: 3,
+			want:     true,
+		},
+		{
+			name:     "found no elements",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: 7,
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Contains(tt.slice, func(val int64) bool {
+				return val == tt.contains
+			})
+			if tt.want != got {
+				t.Errorf("Got %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkContains(b *testing.B) {
+	haystack := []int64{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Contains(haystack, func(val int64) bool {
+			return val == 3
+		})
+	}
+}
+
+func TestContainsAll(t *testing.T) {
+	tests := []struct {
+		name     string
+		slice    []int64
+		contains []int64
+		want     bool
+	}{
+		{
+			name:     "found 1 element",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{3},
+			want:     true,
+		},
+		{
+			name:     "found 3 elements",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{3, 5, 6},
+			want:     true,
+		},
+		{
+			name:     "found some but not all elements",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{3, 5, 6, 7},
+			want:     false,
+		},
+		{
+			name:     "found no elements",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{7, 9},
+			want:     false,
+		},
+		{
+			name:     "found no element",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{7},
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ContainsAll(tt.slice, tt.contains...)
+			if tt.want != got {
+				t.Errorf("Got %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkContainsAll_Single(b *testing.B) {
+	haystack := []int64{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsAll(haystack, 3)
+	}
+}
+
+func BenchmarkContainsAll_Multiple(b *testing.B) {
+	haystack := []int64{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsAll(haystack, 2, 3)
+	}
+}
+
+func TestContainsAtLeastOne(t *testing.T) {
+	tests := []struct {
+		name     string
+		slice    []int64
+		contains []int64
+		want     bool
+	}{
+		{
+			name:     "found 1 element",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{3},
+			want:     true,
+		},
+		{
+			name:     "found 3 elements",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{3, 5, 6},
+			want:     true,
+		},
+		{
+			name:     "found some but not all elements",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{3, 5, 6, 7},
+			want:     true,
+		},
+		{
+			name:     "found no elements",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{7, 9},
+			want:     false,
+		},
+		{
+			name:     "found no element",
+			slice:    []int64{1, 2, 3, 4, 5, 6},
+			contains: []int64{7},
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ContainsAtLeastOne(tt.slice, tt.contains...)
+			if tt.want != got {
+				t.Errorf("Got %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkContainsAtLeastOne_Single(b *testing.B) {
+	haystack := []int64{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsAtLeastOne(haystack, 3)
+	}
+}
+
+func BenchmarkContainsAtLeastOne_Multiple(b *testing.B) {
+	haystack := []int64{1, 2, 3, 4, 5, 6}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ContainsAtLeastOne(haystack, 2, 3)
+	}
+}
--- gofp/map.go
+++ gofp/map.go
@@ -8,3 +8,13 @@ func Map[T, V any](list []T, fn func(T) V) (res []V) {
 
 	return
 }
+
+// MapMultiple apply a function to all elements on an array.
+// Applied function should return multiple elements out of single list element.
+func MapMultiple[T, V any](list []T, fn func(T) []V) (res []V) {
+	for i := range list {
+		res = append(res, fn(list[i])...)
+	}
+
+	return
+}
