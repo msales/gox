2dc5fc88c35130de1c3d36adacc311b22dc84cd2
Henry Sequeda González <henrysg88@gmail.com>
2022-11-23 11:34:06 -0500

    [TASK] TRK-574 Add Set[T] type to gofp (#25)
    * [TASK] TRK-574 Add Set[T] type to gofp
    
    * [TASK] TRK-574 Add ToSliceWithSort method to Set type
    
    * [TASK] TRK-574 Add test for ToSliceWithSort method of gofp.Set type

---
+++ gofp/set.go
@@ -0,0 +1,65 @@
+package gofp
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Set[T] represents a unordered list of unique 'T' elements.
+// NOTE: only works with comparable types because the underlying implementation
+// uses maps and map keys can only be 'comparables'.
+// Should be builded using make. Example:
+// integerSet:= make(Set[int]).
+type Set[T comparable] map[T]struct{}
+
+// Add adds a new element to the set.
+func (s Set[T]) Add(val ...T) {
+	for _, v := range val {
+		s[v] = struct{}{}
+	}
+}
+
+// Has verifies if an element exists in the set.
+func (s Set[T]) Has(val T) bool {
+	_, ok := s[val]
+	return ok
+}
+
+// Remove removes an element from the set.
+func (s Set[T]) Remove(val T) {
+	delete(s, val)
+}
+
+// String implements Stringer
+func (s Set[T]) String() string {
+	keys := make([]T, 0, len(s))
+	for k := range s {
+		keys = append(keys, k)
+	}
+
+	return fmt.Sprintf("%v", keys)
+}
+
+// ToSlice convert the set in a slice.
+func (s Set[T]) ToSlice() []T {
+	keys := make([]T, 0, len(s))
+	for k := range s {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// ToSlice convert the set in a slice applying the sort function.
+func (s Set[T]) ToSliceWithSort(sortFunc func(a, b T) bool) []T {
+	keys := make([]T, 0, len(s))
+
+	for k := range s {
+		keys = append(keys, k)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return sortFunc(keys[i], keys[j])
+	})
+
+	return keys
+}
