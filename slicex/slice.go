8333cada2184844f3c1074e0fcd8c841a107ef53
kubakl <33988980+kubakl@users.noreply.github.com>
2024-08-07 11:23:21 +0200

    Add a generic ContainsAtLeastOne function

--- slicex/slice.go
+++ slicex/slice.go
@@ -37,3 +37,30 @@ func validateReflection(method string, kind reflect.Kind, v reflect.Value) {
 		panic(&reflect.ValueError{Method: method, Kind: v.Kind()})
 	}
 }
+
+// ContainsAtLeastOne checks if at least one element from needles list is in the haystack.
+func ContainsAtLeastOne[T comparable](haystack []T, needles ...T) bool {
+	// Avoid allocations for a single check.
+	if len(needles) == 1 {
+		for _, h := range haystack {
+			if h == needles[0] {
+				return true
+			}
+		}
+		return false
+	}
+
+	checks := make(map[T]struct{}, len(needles))
+	for _, n := range needles {
+		checks[n] = struct{}{}
+	}
+
+	for _, h := range haystack {
+		_, ok := checks[h]
+		if ok {
+			return true
+		}
+	}
+
+	return false
+}
