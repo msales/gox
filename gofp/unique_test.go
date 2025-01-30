fd8e78627fd8b2780d9e1f6d7e662ac2a4124ba1
Henry Sequeda González <henrysg88@gmail.com>
2025-01-29 21:24:25 -0500

    [TASK] TRK-4289 Add UniqueWithKeyBuilder function to gofp

--- gofp/unique_test.go
+++ gofp/unique_test.go
@@ -60,6 +60,36 @@ func TestIsUnique_String(t *testing.T) {
 	}
 }
 
+func TestIsUniqueWithKeyBuilder(t *testing.T) {
+	type data struct {
+		foo string
+		bar []int
+	}
+	type test struct {
+		name string
+		d    []data
+		want bool
+	}
+	tests := []test{
+		{
+			name: "unique",
+			d:    []data{{foo: "m"}, {foo: "x", bar: []int{1}}, {foo: "z", bar: []int{8, 9}}},
+			want: true,
+		},
+		{
+			name: "not unique ",
+			d:    []data{{foo: "m"}, {foo: "x", bar: []int{1}}, {foo: "x", bar: []int{8, 9}}},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := gofp.IsUniqueWithKeyBuilder(tt.d, func(t data) string { return t.foo })
+			assert.Equalf(t, tt.want, got, "Got %+v, want %+v", tt.want, got)
+		})
+	}
+}
+
 func BenchmarkIsUnique_Int(b *testing.B) {
 	slice1 := []int{1, 2, 3, 4, 5}
 
@@ -83,6 +113,39 @@ func BenchmarkIsUnique_Int_Optimistic(b *testing.B) {
 }
 
 func BenchmarkIsUnique_Int_Pessimistic(b *testing.B) {
+	slice1 := []int{1, 2, 3, 4, 5, 1}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		gofp.IsUnique(slice1)
+	}
+}
+
+func BenchmarkIsUniqueWithKeyBuilder_Int(b *testing.B) {
+	slice1 := []int{1, 2, 3, 4, 5}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		gofp.IsUnique(slice1)
+	}
+}
+
+func BenchmarkIsUniqueWithKeyBuilder_Int_Optimistic(b *testing.B) {
+	slice1 := []int{1, 1, 2, 3, 4, 5}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		gofp.IsUnique(slice1)
+	}
+}
+
+func BenchmarkIsUniqueWithKeyBuilder_Int_Pessimistic(b *testing.B) {
 	slice1 := []int{1, 2, 3, 4, 5, 1}
 
 	b.ReportAllocs()
